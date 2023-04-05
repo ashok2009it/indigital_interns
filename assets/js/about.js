ad279b4ca2c250464d752a9fc2ff26ed301522ef
Kiruthiga K <106895503+krithiexplorer@users.noreply.github.com>
2023-04-05 12:38:06 +0530

    Add files via upload
    Attendance management system for checking attendance

---
+++ assets/js/about.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+    reset:true,
+    distance:'60px',
+    duration: 2500,
+    delay:400
+}
+);
+ScrollReveal().reveal('.hero-image-section',{delay:500,origin:'left'});
+ScrollReveal().reveal('.innercontent',{delay:600,origin:'right'});
---
+++ assets/js/pricing.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+    reset:true,
+    distance:'60px',
+    duration: 2500,
+    delay:400
+}
+);
+ScrollReveal().reveal('.details',{delay:500,origin:'left'});
+ScrollReveal().reveal('.box',{delay:600,origin:'right',interval:200});
