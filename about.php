cc5756e372100339031f0d3198b9ff22159ff257
krithiexplorer <kiruthiga.kameswaran@gmail.com>
2023-04-17 13:46:14 +0530

    added php thank you page

---
+++ about.php
@@ -0,0 +1,50 @@
+<?php
+    include 'includes/header.html';
+?>
+ <section>
+            <div class="hero">
+                <div class="hero-image-section">
+                <img class="heroimg" src="https://img.freepik.com/free-vector/collab-concept-illustration_114360-3995.jpg?
+                w=740&t=st=1680081233~exp=1680081833~hmac=f504954a53659de6db25438130888b8e57698033a5c47da1829ea69ccb8074c7" alt="hero">
+                </div>  
+        
+                <div class="herotext">
+                    <div class="innercontent">
+                        <h1>How we stand unique?</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo
+                        r incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.Bacon ipsum dolor amet chicken turducken spare ribs.
+                        </p>
+                    </div>    
+                </div> 
+            </div>
+            <div>
+            <div class="hero">    
+                <div class="herotext">
+                    <div class="innercontent">
+                        <h1>Expertise in CMS</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo
+                            r incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.Bacon ipsum dolor amet chicken turducken spare ribs.
+                        </p>
+                    </div>    
+                </div>
+                <div class="hero-image-section">
+                    <img class="heroimg" src="https://img.freepik.com/free-vector/
+                    girl-makes-post-big-laptop-illustration_335657-273.jpg?
+                    w=740&t=st=1680153456~exp=1680154056~hmac=7
+                    eef1fe65404b3da114e454af739c0da985a439560845fd70c17a0302ab825d9" alt="hero">
+                </div> 
+            </div>    
+            <script src="assets/js/about.js"></script>
+        </section>
+        
+<?php
+    include 'includes/footer.html';
+?>
