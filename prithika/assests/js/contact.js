d259d8da308dc06a4caaa59e7d284d18f1ee3841
Ashok <ashok2009it@gmail.com>
2023-04-24 17:05:42 +0530

    updated prithiga missed files

---
+++ prithika/assests/js/contact.js
@@ -0,0 +1,15 @@
+ScrollReveal({
+    reset:true,
+    distance:'60px',
+    duration: 2500,
+    delay:400
+}
+);
+ScrollReveal().reveal('.title',{delay:90,origin:'top'});
+ScrollReveal().reveal('.text',{delay:90,origin:'bottom'});
+ScrollReveal().reveal('.info',{delay:90,origin:'top'});
+ScrollReveal().reveal('.social-media',{delay:90,origin:'bottom'});
+ScrollReveal().reveal('.contact-form',{delay:90,origin:'top'});
+ScrollReveal().reveal('.text-container',{delay:90,origin:'bottom'});
+ScrollReveal().reveal('.input-container',{delay:90,origin:'bottom'});
+ScrollReveal().reveal('.input',{delay:90,origin:'bottom'});
---
+++ prithika/assests/js/index.js
@@ -0,0 +1,12 @@
+ScrollReveal({
+    reset:true,
+    distance:'60px',
+    duration: 2500,
+    delay:400
+}
+);
+ScrollReveal().reveal('.content',{delay:90,origin:'top'});
+ScrollReveal().reveal('.clss',{delay:90,origin:'bottom'});
+ScrollReveal().reveal('.our_service_section',{delay:90,origin:'top'});
+ScrollReveal().reveal('.single_service_container',{delay:90,origin:'bottom'});
+ScrollReveal().reveal('.attach',{delay:90,origin:'bottom'});
